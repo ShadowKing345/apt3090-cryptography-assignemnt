e1b1d0322344ce532688797f6ac2425f2088c3cd
Alex <alexandruturcanu915@gmail.com>
2020-02-14 16:40:59 +0300

    Inital commit.

---
+++ app/src/main/java/com/example/aes/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.aes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
